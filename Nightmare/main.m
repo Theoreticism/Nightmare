5a04826fd1cef182e8854ad867ef2f1196962662
Marist User <maristuser@hc0004m-03.local>
2014-10-08 20:56:12 -0400

    Initial Commit

---
+++ Nightmare/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Nightmare
+//
+//  Created by Marist User on 10/8/14.
+//  Copyright Marist User 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
